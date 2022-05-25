8beb9042b2edd7789ee61679370d17bcff2b6b81
changcarlson@hotmail.com <USEREMAIL>
2022-05-25 12:13:22 -0700

    task2 final commit

---
+++ dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/basic.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/basic.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/cca.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/cca.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/cnn_face_detector.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/cnn_face_detector.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/correlation_tracker.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/correlation_tracker.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/decision_functions.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/decision_functions.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/dlib.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/dlib.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/face_recognition.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/face_recognition.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/global_optimization.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/global_optimization.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/image.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/image.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/image2.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/image2.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/image3.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/image3.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/image4.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/image4.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/image_dataset_metadata.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/image_dataset_metadata.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/line.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/line.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/matrix.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/matrix.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/numpy_returns.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/numpy_returns.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/object_detection.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/object_detection.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/other.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/other.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/rectangles.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/rectangles.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/sequence_segmenter.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/sequence_segmenter.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/shape_predictor.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/shape_predictor.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/svm_c_trainer.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/svm_c_trainer.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/svm_rank_trainer.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/svm_rank_trainer.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/svm_struct.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/svm_struct.cpp.o"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/tools/python/src/vector.cpp" "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/CMakeFiles/dlib_python.dir/src/vector.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DLIB_VERSION=19.17.0"
+  "dlib_python_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/dlib/external/pybind11/include"
+  "/usr/include/python3.8"
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/dlib/.."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/student/Desktop/CSI_6_ART_CW2/dlib-19.17.0/build/temp.linux-x86_64-3.8/dlib_build/CMakeFiles/dlib.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
